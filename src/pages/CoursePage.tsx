c5656ac0efa8463a50b30ad6dba00662780ad534
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2023-03-18 16:32:01 +0100

    added video component

--- src/pages/CoursePage.tsx
+++ src/pages/CoursePage.tsx
@@ -1,26 +1,39 @@
+import { Row, Col } from 'antd';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { getCourseApi } from '../api/course-api';
 import { Course } from '../dto/Course';
+import ReactPlayer from 'react-player'
+import LessonsOfTheCourse from '../components/LessonsOfTheCourse';
+import { STUB_COURSE_DETAILS } from '../api/data-course-details';
+import '../components/styles/courseContentStyle.css'
 
 export const CoursePage = () => {
 	const { id } = useParams();
-	const [course, setCourse] = useState<Course | null>(null)
+	const course = STUB_COURSE_DETAILS;
+	// const [course, setCourse] = useState<Course | null>(null)
 
-	useEffect(() => {
-		async function getCourse() {
-			const course = await getCourseApi(id!);
-			setCourse(course)
-		}
-		getCourse();
+	// useEffect(() => {
+	// 	async function getCourse() {
+	// 		const course = await getCourseApi(id!);
+	// 		setCourse(course)
+	// 	}
+	// 	getCourse();
 
-	}, [id]);
+	// }, [id]);
 
 	return (
-		<div className='app-content'>
-			<div>I'm course {id}
-				<div>I'm course {course?.title}</div>
+		<main className='course-page-app-content'>
+			<h1 className='course-page-title'>{course?.title}</h1>
+			<div className='course-page-description'>{course?.description}</div>
+			<div className='course-page-video-lessons'>
+
+				<ReactPlayer
+					url={course?.meta.courseVideoPreview?.link}
+					controls={true}
+				/>
 			</div>
-		</div>
+
+		</main>
 	)
 }
