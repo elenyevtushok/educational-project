0d8f177b8865f49d087ced2230abb6faad5cbb14
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2023-03-19 14:59:54 +0100

    Fix error problem

--- src/components/common/ErrorFallback.tsx
+++ src/components/common/ErrorFallback.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-export const ErrorFallback = ({ error }) => {
+export const ErrorFallback = () => {
   return (
 	  <div role="alert">
 		  <p>Something went wrong:</p>
-		  <pre>{error}</pre>
-		  {/* <button onClick={resetErrorBoundary}>Try again</button> */}
 	  </div>
   )
 }
